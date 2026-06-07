d085b890cdb4e5cdf67bd8f2e96f71a8733ddf26
allyxaaa <allyzamaemagsipoc01@gmail.com>
2026-06-07 18:11:27 +0800

    fix: set correct import path for AuthProvider

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-<<<<<<< HEAD
 import { AuthProvider } from './hooks/useAuth.jsx'
-=======
-import { AuthProvider } from './context/useAuth.jsx'
->>>>>>> dev-avery
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -14,8 +10,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <App />
     </AuthProvider>
   </React.StrictMode>,
-<<<<<<< HEAD
 )
-=======
-)
->>>>>>> dev-avery
